703219cfab70c8b01af761b77fd6f155496eaeaf
Nikolas <nikmaehl@uni-bremen.de>
2018-03-25 23:35:14 +0200

    hlint

--- app/Main.hs
+++ app/Main.hs
@@ -2,17 +2,13 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE TypeFamilies #-}
 
-import Data.Fixed
 import Data.List.Split
-import qualified Data.Map as Map hiding (map)
+import qualified Data.Map as Map
 import Data.Maybe
-import Diagrams.Backend.CmdLine
 import Diagrams.Backend.SVG
-import Diagrams.Backend.SVG.CmdLine
 import Diagrams.Prelude
-import System.Environment
 import qualified Options as Opt
-import Control.Applicative
+
 
 data HeatmapOptions = HeatmapOptions 
     { optOutfile :: FilePath
@@ -23,7 +19,7 @@ data HeatmapOptions = HeatmapOptions
 instance Opt.Options HeatmapOptions where
     defineOptions = HeatmapOptions
         <$> Opt.simpleOption "out" "out.svg" "File to save the generated heatmap."
-        <*> Opt.simpleOption "clusterRadius" 0.2 "Radius of a circle which is used to cluster and display positions."
+        <*> Opt.simpleOption "clusterRadius" 0.1 "Radius of a circle which is used to cluster and display positions."
         <*> Opt.simpleOption "maxTemperature" 10 "Maximal number of positions in a cluster."
 
 main = Opt.runCommand $ \opts (filename:_) -> do
@@ -44,22 +40,22 @@ collectPositions options (p:ps) = Map.insert convertedPos (currentAmount + 1) pr
         [x, y] = splitOn "," p
 
 processPosition :: HeatmapOptions -> String -> Int
-processPosition options s = round ((read s) / (optClusterRadius options * 2))
+processPosition options s = round (read s / (optClusterRadius options * 2))
 
 createCircle :: HeatmapOptions -> (Int, Int) -> Int -> Diagram B
 createCircle options (x, y) n =
-    circle (circleRadius) 
+    circle circleRadius
         # translate (r2 (xPos, yPos)) 
         # lw 0 
         # fc (sRGB24 0 0 0) 
         # opacity (calculateOpacity options n)
   where
     circleRadius = optClusterRadius options
-    xPos = (fromIntegral x) * circleRadius * 2
-    yPos = (fromIntegral y) * circleRadius * 2
+    xPos = fromIntegral x * circleRadius * 2
+    yPos = fromIntegral y * circleRadius * 2
 
 mergeDiagramms :: [Diagram B] -> Diagram B
 mergeDiagramms = foldl1 atop
 
 calculateOpacity :: HeatmapOptions -> Int -> Double
-calculateOpacity options n = 0.1 + (0.9 / fromIntegral (optMaxTemperature options)) * (fromIntegral n)
+calculateOpacity options n = 0.1 + (0.9 / fromIntegral (optMaxTemperature options)) * fromIntegral n
