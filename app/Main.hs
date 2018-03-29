570a64a4f823ebbc7e93dfdb2a90715bf4c18c88
Nikolas <nikmaehl@uni-bremen.de>
2018-03-30 00:19:33 +0200

    added Reader Monad

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE TypeFamilies #-}
 
+import Control.Monad.Reader
 import Data.List.Split
 import qualified Data.Map as Map
 import Data.Maybe
@@ -25,37 +26,42 @@ instance Opt.Options HeatmapOptions where
 main = Opt.runCommand $ \opts (filename:_) -> do
     content <- readFile filename
     let positions = lines content
-    let positionsMap = collectPositions opts positions
-    let positionCircles = map (\(p, n) -> createCircle opts p n) (Map.toList positionsMap)
+        positionsMap = runReader (collectPositions positions) opts
+        positionCircles = map (\(p, n) -> runReader (createCircle p n) opts) (Map.toList positionsMap)
     renderSVG (optOutfile opts) (mkWidth 1000) (mergeDiagramms positionCircles)
 
-collectPositions :: HeatmapOptions -> [String] -> Map.Map (Int, Int) Int
-collectPositions _ [] = Map.empty
-collectPositions options (p:ps) = Map.insert convertedPos (currentAmount + 1) previousMap
-  where
-    currentAmount = fromMaybe 0 (Map.lookup convertedPos previousMap)
-    previousMap = collectPositions options ps
-    convertedPos = (processPosition options x, processPosition options y)
-      where
-        [x, y] = splitOn "," p
+collectPositions :: [String] -> Reader HeatmapOptions (Map.Map (Int, Int) Int)
+collectPositions [] = return Map.empty
+collectPositions (p:ps) = do
+    previousMap <- collectPositions ps
+    let [x, y] = splitOn "," p
+    convertedX <- processPosition x
+    convertedY <- processPosition y
+    let convertedPos = (convertedX, convertedY)
+        currentAmount = fromMaybe 0 (Map.lookup convertedPos previousMap)
+    return $ Map.insert convertedPos (currentAmount + 1) previousMap
 
-processPosition :: HeatmapOptions -> String -> Int
-processPosition options s = round (read s / (optClusterRadius options * 2))
+processPosition :: String -> Reader HeatmapOptions Int
+processPosition s = do
+    clusterRadius <- reader optClusterRadius
+    return $ round (read s / (clusterRadius * 2))
 
-createCircle :: HeatmapOptions -> (Int, Int) -> Int -> Diagram B
-createCircle options (x, y) n =
-    circle circleRadius
+createCircle :: (Int, Int) -> Int -> Reader HeatmapOptions (Diagram B)
+createCircle (x, y) n = do
+    circleRadius <- reader optClusterRadius
+    calcOpacity <- calculateOpacity n
+    let xPos = fromIntegral x * circleRadius * 2
+        yPos = fromIntegral y * circleRadius * 2
+    return $ circle circleRadius
         # translate (r2 (xPos, yPos)) 
         # lw 0 
         # fc (sRGB24 0 0 0) 
-        # opacity (calculateOpacity options n)
-  where
-    circleRadius = optClusterRadius options
-    xPos = fromIntegral x * circleRadius * 2
-    yPos = fromIntegral y * circleRadius * 2
+        # opacity calcOpacity
 
 mergeDiagramms :: [Diagram B] -> Diagram B
 mergeDiagramms = foldl1 atop
 
-calculateOpacity :: HeatmapOptions -> Int -> Double
-calculateOpacity options n = 0.1 + (0.9 / fromIntegral (optMaxTemperature options)) * fromIntegral n
+calculateOpacity :: Int -> Reader HeatmapOptions Double
+calculateOpacity n = do
+    maxTemperature <- reader optMaxTemperature
+    return $ 0.1 + (0.9 / fromIntegral maxTemperature) * fromIntegral n
